fb17c077aa7c53c3d8902493cd0787d24b85db87
Ykbmo <inruhr@gmail.com>
2020-12-03 09:44:48 +0900

    add team name

---
+++ chams.py
@@ -0,0 +1,20 @@
+
+team_name = {'Bayern Muenchen' : ['A',1,'Germany'],
+             'RB Salsburg' : ['A',2,'Austria'],     #or ATM
+             'Borussia Moenchengladbach' :['B',1,'Germany'], #B
+             'Inter Milan' : ['B',2,'Italy'], #B
+             'Manchester City' : ['C',1,'England'],
+             'Porto' : ['C',2,'Portugal'],
+             'Liverpool' :['D',1,'England'],
+             'Ajax' : ['D',2,'Netherland'], #or Atalanta
+             'Chelsea' : ['E',1,'England'],
+             'Sevilla' : ['E',2,'Spain'],
+             'Borussia Dortmund' : ['F',1,'Germany'], # or 2
+             'Lazio':['F',2,'Italy'], # or Club Brugge
+             'Barcelona' : ['G',1,'Spain'], # or 2
+             'Juventus' : ['G',2,'Italy'], # or 1
+             'RB Leipzig' : ['H',1,'Germany'], # or ManU
+             'Paris Saint-Germain' : ['H',2,'France']}
+             
+    
+print(team_name['Bayern Muenchen'])
